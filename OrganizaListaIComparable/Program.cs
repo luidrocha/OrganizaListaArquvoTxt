5561751f669d695432048d1acffcb1488c203663
Luidrocha <luidrocha@yahoo.com.br>
2021-12-15 19:05:08 -0300

    Organiza lista lida no arquivo texte  sem Comparable CSHARP

---
+++ OrganizaListaIComparable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace OrganizaListaIComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\lista.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    List<String> list = new List<string>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+
+                    list.Sort();
+
+                    foreach(string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Correu um Erro: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
